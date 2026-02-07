c0f0519e2aa2beafb3b8d71794952e96a2997b31
Eduard <eduuardcovrig@gmail.com>
2026-02-07 22:26:00 +0200

    Refactor front, change front from js to ts, add shadcn/ui

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background font-sans antialiased">
+      {/* Aici va veni Router-ul (Harta site-ului) */}
+      <h1 className="text-2xl p-5 font-bold">EdwC Store - Ready to build</h1>
+    </div>
+  )
+}
+
+export default App
