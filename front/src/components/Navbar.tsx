7ec82cfceaaa8b749c71724d296446c8069be0a3
Eduard <eduuardcovrig@gmail.com>
2026-02-09 17:18:04 +0200

    add login page, and example for .env to link the url of the springboot api

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
-import {Link} from "react-router-dom"
+import {Link, useLocation   } from "react-router-dom"
 import {ShoppingCart, User, Search} from "lucide-react" //iconitele
 
 export default function Navbar()
 {
+    const location=useLocation(); //pentru a afla pe ce pagina suntem acum.
+    if(location.pathname==="/login" || location.pathname==="/register")
+        return null;
     return (
         <nav className="flex items-center justify-between px-8 py-4 bg-white border-b border-gray-200">
         {/* ZONA 1: LOGO (Stanga) */}
@@ -20,13 +23,13 @@ export default function Navbar()
             {/* Buton User */}
             <Link to="/login" className="flex items-center gap-1 text-gray-600 hover:text-blue-700 font-medium">
             <User size={20}/>
-            <span className="hidden sm:inline">My Account</span> {/* default hidden, insa incepand cu ecranele care se incadreaza
+                <span className="hidden sm:inline">My Account</span> {/* default hidden, insa incepand cu ecranele care se incadreaza
             in small se afiseaza ce e in inline adica My Account, lasa doar iconita */}
             </Link>
             {/* Buton Cos */}
             <Link to="/cart" className="relative bg-blue-50 p-2 text-blue-600 rounded-full hover:bg-blue-100 transition">
-            <ShoppingCart size={20}/>
-            <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">0</span>
+                <ShoppingCart size={20}/>
+                <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">0</span>
             </Link>
 
         </div>
--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,4 +1,90 @@
-export default function Login()
-{
-    return <div className="px-10 text-2xl font-bold">Aici e login-ul</div>
+import { Link, useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Loader2, X } from "lucide-react";
+import { useState } from "react"
+import axios from "axios";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
+    const isValid = email.trim().length > 0 && password.trim().length > 0; // trim elimina space-urile de la inceput si sfarsit.
+    const navigate = useNavigate();
+
+    const handleLogin = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const apiUrl = import.meta.env.VITE_API_URL;
+        try {
+            const response = await axios.post(`${apiUrl}/auth/login`,
+                {
+                    email: email,
+                    password: password
+                });
+            console.log("Raspuns de la server:", response.data);
+            // salvam tokenul ulterior
+            navigate("/");
+        }
+        catch (error: any) {
+            console.error("Eroare la login:", error);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4 pb-60">
+            <Card className="w-full max-w-lg shadow-blue-300 shadow-md relative p-8">
+                <Link to="/" className="absolute top-4 right-4 text-gray-400 hover:bg-gray-200 hover:text-gray-900 transition">
+                    <X size={28} />
+                </Link>
+                <CardHeader className="space-y-1 text-center">
+                    <CardTitle className="text-3xl font-bold "> Welcome back!
+                    </CardTitle>
+                    <CardDescription>Please enter your credentials to access your account.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleLogin} className="space-y-4">
+                        {/*Email */}
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" placeholder="name@example.com" value={email}
+                                onChange={(e) => setEmail(e.target.value)} required className="h-12 hover:border-gray-500 transition-colors duration-300" />
+                        </div>
+                        {/*Password */}
+                        <div className="space-y-2">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="password" placeholder="Your password" value={password}
+                                onChange={(e) => setPassword(e.target.value)} required className="h-12 hover:border-gray-500 transition-colors duration-300" />
+                            <div className="flex justify-end">
+                                <Link to="#" className="text-sm text-gray-400 hover:underline">Forgot password?</Link>
+                            </div>
+                        </div>
+                        <Button disabled={!isValid || isLoading} type="submit" className={`w-full h-12 transition-colors duration-200 ${isValid ?
+                                "bg-blue-400 hover:bg-blue-500 text-white"  //valid
+                                : "bg-gray-200 text-gray-400 cursor-not-allowed" //invalid
+                            }`}>
+                            {isLoading ? (
+                                <>
+                                    <Loader2 className="mr-2 h-4 w-8 animate-spin" />
+                                </>
+                            ) : (
+                                "Login"
+                            )}
+                        </Button>
+
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-center">
+                    <p className="text-sm text-gray-500">Don't have an account yet?
+                        <Link to="/register" className="text-blue-600 hover:underline ml-1 font-normal">Sign up</Link>
+                    </p>
+                </CardFooter>
+            </Card>
+        </div>
+    )
 }
